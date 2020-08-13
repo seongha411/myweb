4c153e5ea5d96a35ef70e5d873b60c23ec1e701c
SeongHa <seongha411@gmail.com>
2020-08-13 19:08:35 +0900

    2nd

--- src/App.js
+++ src/App.js
@@ -1,40 +1,82 @@
 import React from 'react';
-import {Button, Progress, Input} from 'semantic-ui-react'
+import {Image, Comment, Form, Button, Header} from 'semantic-ui-react'
 
-function Login2() {
-  return(
-    <div>
-    <Input/>
-    <Button onClick = { (e) => {
-      console.log(e)
-      alert(e.target.value)
-    }}> 로그인 </Button>
-    </div>
-  )
-}
-
-class Login extends React.Component {
-
-  render(){
-    return(
-      <div>
-      <Input/>
-      <Button onClick = { (e) => {
-        console.log(e)
-        alert(e.target.value)
-      } } > Login </Button>
-      </div>
-    )
-  }
-}
+import imagefile from "./campus_map_01.png"
+import human from "./human.png"
 
 function App() {
   return (
     <div>
-      <p> 안녕하세요!! </p>
-      <Button primary> 하이하이~ </Button>
-      <Progress percent = {90} indicating/>
-      <Login / >
+      <Image src = {imagefile} centered />
+      <Comment.Group>
+  <Header as='h3' dividing>
+    Comments
+  </Header>
+
+  <Comment>
+    <Comment.Avatar src={human} />
+    <Comment.Content>
+      <Comment.Author as='a'>Matt</Comment.Author>
+      <Comment.Metadata>
+        <div>Today at 5:42PM</div>
+      </Comment.Metadata>
+      <Comment.Text>How artistic!</Comment.Text>
+      <Comment.Actions>
+        <Comment.Action>Reply</Comment.Action>
+      </Comment.Actions>
+    </Comment.Content>
+  </Comment>
+
+  <Comment>
+    <Comment.Avatar src={human} />
+    <Comment.Content>
+      <Comment.Author as='a'>Elliot Fu</Comment.Author>
+      <Comment.Metadata>
+        <div>Yesterday at 12:30AM</div>
+      </Comment.Metadata>
+      <Comment.Text>
+        <p>This has been very useful for my research. Thanks as well!</p>
+      </Comment.Text>
+      <Comment.Actions>
+        <Comment.Action>Reply</Comment.Action>
+      </Comment.Actions>
+    </Comment.Content>
+    <Comment.Group>
+      <Comment>
+        <Comment.Avatar src={human} />
+        <Comment.Content>
+          <Comment.Author as='a'>Jenny Hess</Comment.Author>
+          <Comment.Metadata>
+            <div>Just now</div>
+          </Comment.Metadata>
+          <Comment.Text>Elliot you are always so right :)</Comment.Text>
+          <Comment.Actions>
+            <Comment.Action>Reply</Comment.Action>
+          </Comment.Actions>
+        </Comment.Content>
+      </Comment>
+    </Comment.Group>
+  </Comment>
+
+  <Comment>
+    <Comment.Avatar src={human} />
+    <Comment.Content>
+      <Comment.Author as='a'>Joe Henderson</Comment.Author>
+      <Comment.Metadata>
+        <div>5 days ago</div>
+      </Comment.Metadata>
+      <Comment.Text>Dude, this is awesome. Thanks so much</Comment.Text>
+      <Comment.Actions>
+        <Comment.Action>Reply</Comment.Action>
+      </Comment.Actions>
+    </Comment.Content>
+  </Comment>
+
+  <Form reply>
+    <Form.TextArea />
+    <Button content='Add Reply' labelPosition='left' icon='edit' primary />
+  </Form>
+</Comment.Group>
     </div>
   );
 }
