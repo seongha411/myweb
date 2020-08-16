ea2d78c842cfb8788dde5a61b8289173e627f5c1
SeongHa <seongha411@gmail.com>
2020-08-16 14:56:01 +0900

    Code

--- src/login.js
+++ src/login.js
@@ -1,29 +1,40 @@
 function Login3() {
-  return(
+  return (
     <div>
-    <Input/>
-    <Button onClick = { (e) => {
-      console.log(e)
-      alert(e.target.value)
-    }}> 로그인 </Button>
+      <Input />
+      <Button
+        onClick={(e) => {
+          console.log(e);
+          alert(e.target.value);
+        }}
+      >
+        {" "}
+        로그인{" "}
+      </Button>
     </div>
-  )
+  );
 }
 
 class Login extends React.Component {
-  constructor(){
-    super()
-    this.state = {id: "아무것도 입력을 안했구만!"}
+  constructor() {
+    super();
+    this.state = { id: "아무것도 입력을 안했구만!" };
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div>
-      <Input onChange = { (e) => this.setState({id : e.target.value })}/>
-      <Button value = {this.state.id} onClick = { (event) => {
-        alert(this.state.id)
-      } } > Login </Button>
+        <Input onChange={(e) => this.setState({ id: e.target.value })} />
+        <Button
+          value={this.state.id}
+          onClick={(event) => {
+            alert(this.state.id);
+          }}
+        >
+          {" "}
+          Login{" "}
+        </Button>
       </div>
-    )
+    );
   }
 }
