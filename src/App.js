ddfea34f3b9b2341baef16d3a1477ac9c399f66b
SeongHa <seongha411@gmail.com>
2020-08-18 21:18:48 +0900

    Login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import firebase from "firebase";
 import {
   Image,
   Divider,
@@ -12,85 +13,126 @@ import {
 
 import Comments from "./comment.js";
 import Buttons from "./buttons.js";
+import auth from "./fb.js";
 
 import campusmap from "./campus_map_01.png";
 
-function App() {
-  return (
-    <div style={{ backgroundColor: "black" }}>
-      <h2 style={{ color: "white", textAlign: "center", paddingTop : "15px" }}> SeongHa's Web </h2>
-      <Menu inverted widths={3}>
-        <Menu.Item> Menu </Menu.Item>
-        <Menu.Item> 반가워요, 방문자님! </Menu.Item>
-        <Menu.Item> Login </Menu.Item>
-      </Menu>
-      <Divider horizontal>
-        <Header as="h4" style={{ color: "white" }}>
-          My Project
-        </Header>
-      </Divider>
-      <Grid centered>
-        <Grid.Row>
-          <Image src={campusmap} centered />
-        </Grid.Row>
-        <Grid.Row>
-          <Buttons />
-        </Grid.Row>
-      </Grid>
+var provider = new firebase.auth.GoogleAuthProvider();
 
-      <Divider horizontal>
-        <Header as="h4" style={{ color: "white" }}>
-          <Icon name="comment alternate" />
-          Comment
-        </Header>
-      </Divider>
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userName: "방문자",
+    };
+  }
+  render() {
+    return (
+      <div style={{ backgroundColor: "black" }}>
+        <h2 style={{ color: "white", textAlign: "center", paddingTop: "15px" }}>
+          {" "}
+          SeongHa's Web{" "}
+        </h2>
+        <Menu inverted widths={3}>
+          <Menu.Item> Menu </Menu.Item>
+          <Menu.Item> {`반가워요, ${this.state.userName}님!`} </Menu.Item>
+          <Menu.Item
+            onClick={() => {
+              firebase
+                .auth()
+                .signInWithPopup(provider)
+                .then(function (result) {
+                  // This gives you a Google Access Token. You can use it to access the Google API.
+                  var token = result.credential.accessToken;
+                  // The signed-in user info.
+                  var user = result.user;
+                  return user.displayName;
+                  // ...
+                })
+                .then((result) => this.setState({ userName: result }))
+                .catch(function (error) {
+                  // Handle Errors here.
+                  var errorCode = error.code;
+                  var errorMessage = error.message;
+                  // The email of the user's account used.
+                  var email = error.email;
+                  // The firebase.auth.AuthCredential type that was used.
+                  var credential = error.credential;
+                  // ...
+                });
+            }}
+          >
+            {" "}
+            Login{" "}
+          </Menu.Item>
+        </Menu>
+        <Divider horizontal>
+          <Header as="h4" style={{ color: "white" }}>
+            My Project
+          </Header>
+        </Divider>
+        <Grid centered>
+          <Grid.Row>
+            <Image src={campusmap} centered />
+          </Grid.Row>
+          <Grid.Row>
+            <Buttons />
+          </Grid.Row>
+        </Grid>
 
-      <Grid centered columns={3}>
-        <Grid.Column>
-          <Comments />
-        </Grid.Column>
-      </Grid>
+        <Divider horizontal>
+          <Header as="h4" style={{ color: "white" }}>
+            <Icon name="comment alternate" />
+            Comment
+          </Header>
+        </Divider>
 
-      <br />
-      <Divider horizontal>
-        <Header as="h4" style={{ color: "white" }}>
-          <Icon name="mail" style={{ color: "white" }} />
-          Contact Me
-        </Header>
-      </Divider>
-      <br />
-      <Grid centered>
-        <Button
-          circular
-          color="facebook"
-          icon="facebook"
-          onClick={() => window.open("https://fb.me/seongha411")}
-        />
-        <Button
-          circular
-          color="instagram"
-          icon="instagram"
-          onClick={() => window.open("https://instagram.com/seongha411")}
-        />
-        <Button
-          circular
-          color="youtube"
-          icon="youtube"
-          onClick={() =>
-            window.open(
-              "https://www.youtube.com/channel/UCRecMBqmukRIHqMpI1Tqa1Q"
-            )
-          }
-        />
-        <Button
-          circular
-          color="google plus"
-          icon="google plus"
-          onClick={() => alert("준비중입니다.")}
-        />
-      </Grid>
-    </div>
-  );
+        <Grid centered columns={3}>
+          <Grid.Column>
+            <Comments />
+          </Grid.Column>
+        </Grid>
+
+        <br />
+        <Divider horizontal>
+          <Header as="h4" style={{ color: "white" }}>
+            <Icon name="mail" style={{ color: "white" }} />
+            Contact Me
+          </Header>
+        </Divider>
+        <br />
+        <Grid centered>
+          <Button
+            circular
+            color="facebook"
+            icon="facebook"
+            onClick={() => window.open("https://fb.me/seongha411")}
+          />
+          <Button
+            circular
+            color="instagram"
+            icon="instagram"
+            onClick={() => window.open("https://instagram.com/seongha411")}
+          />
+          <Button
+            circular
+            color="youtube"
+            icon="youtube"
+            onClick={() =>
+              window.open(
+                "https://www.youtube.com/channel/UCRecMBqmukRIHqMpI1Tqa1Q"
+              )
+            }
+          />
+          <Button
+            circular
+            color="google plus"
+            icon="google plus"
+            onClick={() => alert("준비중입니다.")}
+          />
+        </Grid>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/fb.js
@@ -0,0 +1,18 @@
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDnGHFS34zqyKqeWF_X5P0YN1Spk_OupSw",
+  authDomain: "seonghaweb.firebaseapp.com",
+  databaseURL: "https://seonghaweb.firebaseio.com",
+  projectId: "seonghaweb",
+  storageBucket: "seonghaweb.appspot.com",
+  messagingSenderId: "667198123593",
+  appId: "1:667198123593:web:ab5a02920eca7a30b59998",
+  measurementId: "G-TMWB0WYKYY",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+
+export default auth;
